02078105510d3c7ec386b9f696e02a0c81e2a5d7
hsy <hsy@dd.com>
2015-06-02 16:39:16 +0800

    save

---
+++ test4uiBuilder/src/com/didi/biz/editor/container/RequestCode.java
@@ -0,0 +1,13 @@
+package com.didi.biz.editor.container;
+
+/**
+ * Class description
+ * 
+ * @author houshengyong
+ * @since 2015-6-2
+ */
+
+public class RequestCode {
+    private static int index = 0;
+    public static final int REQUEST_CODE_PICK_PASSANGER = index++;
+}
