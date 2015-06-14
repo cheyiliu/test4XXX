409a574cd5a6c9bc35a5c520fc6c0b002ede9445
do <cheyiliu@163.com>
2015-06-14 10:38:32 +0800

    add a noisy test case

--- test4observer/src/test4observer/MainTest.java
+++ test4observer/src/test4observer/MainTest.java
@@ -3,10 +3,26 @@ package test4observer;
 public class MainTest {
 
 	public static void main(String[] args) {
-		basketball();
+		normalCase();
+		noisyCase();
 	}
 
-	private static void basketball() {
+	private static void noisyCase() {
+		System.out.println("noisyCase__________________________");
+		ContextNoisy contextNormal = new ContextNoisy();
+		IPerson person1 = new BasketballPlayer(contextNormal, "路人甲");
+		IPerson person2 = new BasketballPlayer(contextNormal, "路人乙");
+
+		contextNormal.register(person1);
+		contextNormal.register(person2);
+
+		person1.say("传球给我");
+		person2.say("你自个玩吧");
+		contextNormal.unRegister(person2);
+	}
+
+	private static void normalCase() {
+		System.out.println("normalCase__________________________");
 		ContextNormal contextNormal = new ContextNormal();
 		IPerson person1 = new BasketballPlayer(contextNormal, "路人甲");
 		IPerson person2 = new BasketballPlayer(contextNormal, "路人乙");
