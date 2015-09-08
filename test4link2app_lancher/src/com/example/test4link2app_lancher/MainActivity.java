092d0f86f7926b5feb60b1623792356b77ecb93e
hsy <houshengyong@diditaxi.com.cn>
2015-09-08 15:06:12 +0800

    add test link 2 app

---
+++ test4link2app_lancher/src/com/example/test4link2app_lancher/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.test4link2app_lancher;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView wv = (WebView) findViewById(R.id.webview);
+        wv.loadUrl("file:///android_asset/wst.html"); 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
